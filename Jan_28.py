2a21cd59bbab911021570ac56b939587818113c2
Jasanpreet Singh Sidhu <jasanpreetsinghsidhu@gmail.com>
2025-01-28 20:43:06 -0500

    welcome_student program and display_sum program

---
+++ Jan_28.py
@@ -0,0 +1,33 @@
+"""
+Write a program that:
+ a. Prompts the user to enter their name.
+ b. Displays a message saying, "Welcome to PASS, [NAME]", where [NAME] is
+ the name entered by the user.
+"""
+
+# prompts the user to enter their name
+user = input("Enter your name: ")
+
+# displays the welcome message
+print("Welcome to PASS,", user)
+
+
+
+
+
+
+
+
+
+
+
+"""
+Write a program that asks the user for two numbers
+and displays the sum of those two numbers
+"""
+                
+# asks the user for two numbers
+x = input("Enter number 1: ")
+y = input("Enter number 2: ")
+
+# will continue writing this program in next class
