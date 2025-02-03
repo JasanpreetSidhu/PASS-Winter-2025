770614aa47bd4ad5e02cd9ad3dd7222796827173
Jasanpreet Singh Sidhu <jasanpreetsinghsidhu@gmail.com>
2025-02-03 16:17:16 -0500

    Feb3 Programs

---
+++ Feb3 Add Two Numbers.py
@@ -0,0 +1,13 @@
+# prompt the user for input 2 numbers
+userInput1 = input("Enter number 1: ")
+userInput2 = input("Enter number 2: ")
+
+# convert userInput to int
+num1 = int(userInput1)
+num2 = int(userInput2)
+
+# calculate the sum
+total =  num1 + num2
+
+# display the sum
+print("Total: ", total)
---
+++ Feb3 PASS Student Greeting.py
@@ -0,0 +1,12 @@
+# variable
+is_new_student = False
+
+# prompts the user to enter their name
+user = input("Enter your name: ")
+
+# CONTINUE ..............
+if (is_new_student)
+
+
+# displays the welcome message
+print("Welcome to PASS,", user)
