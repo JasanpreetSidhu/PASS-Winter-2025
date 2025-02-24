a06ee299faca4832fe81b49ddca87a67c25b2f9c
Jasanpreet Singh Sidhu <jasanpreetsinghsidhu@gmail.com>
2025-02-24 16:20:08 -0500

    Feb24 Programs

---
+++ Feb24 if statements refresher.py
@@ -0,0 +1,17 @@
+# check flow chart for better understanding of errors inside questions
+
+# question 1
+num = -5
+if num > 8:
+    print("Greater than 8")
+else:
+    if num > 5:
+        print("Greater than 5")
+
+
+# question 2
+x = 25
+if x > 10 and x < 20:
+    print("Between 10 and 20")
+else:
+    print("Out of range")
