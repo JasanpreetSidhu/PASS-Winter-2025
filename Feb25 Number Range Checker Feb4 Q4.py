294d8d7adeb9f3707c2158b8d8aabedfa1eb0f41
Jasanpreet Singh Sidhu <jasanpreetsinghsidhu@gmail.com>
2025-02-25 14:12:52 -0500

    Feb25 Programs

---
+++ Feb25 Number Range Checker Feb4 Q4.py
@@ -0,0 +1,11 @@
+number = input("Enter a number: ");
+# changes datatype from string to int
+number = int(number)
+if number > 10 and number < 20:
+    print("Inside range")
+else:
+    if number > 30 and number < 40:
+        print("Inside range")
+    else:
+        print("Outside range")
+    
